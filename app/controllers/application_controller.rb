08a496a8a7fbbaf16d43964e0e1a5be6808dffac
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-02 10:41:41 -0500

    users-sessions controllers auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
+    def current_user
+        def current_user
+            @current_user ||= User.find_by_id(session[:user_id])
+        end
+    end
+
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: { error: "Wrong username or password 😕" }, status: :not_found
+        end
+    end
+
+    def destroy
+        if current_user
+            session.delete :user_id
+        else 
+            render json: { error: "No active session" }, status: :unprocessable_entity
+        end
+    end
+
+end
