834bca061c18a23644a1751ea5cff18c34d737b4
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-03 09:34:32 -0500

    NavLinks with react router

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 
 function NavBar({ currentUser, setCurrentUser }) {
 
@@ -8,16 +9,17 @@ function NavBar({ currentUser, setCurrentUser }) {
     }
 
     return( 
-
-        <ul className="navbar">
-            <li id="nav-logo"><a>Spartan Rivalries</a></li>
-            <li>
-                <button onClick={handleClick}>Logout</button>
-            </li>
-            <li> Hello, {currentUser.username}!</li>
-            <li><a>My Favorites</a></li>
-            <li><a>Rivals</a></li>  
-        </ul>
+        <nav>
+            <ul className="navbar">
+                <li className="logo"><NavLink to="/about">Spartan Rivalries</NavLink></li>
+                <li className="item">
+                    <button onClick={handleClick}>Logout</button>
+                </li>
+                <li className="item"><NavLink to={`/${currentUser.username}`}> Hello, {currentUser.username}!</NavLink></li>
+                <li className="item"><NavLink to="/favorites">My Favorites</NavLink></li>
+                <li className="item"><NavLink to="rivalry">Rivals</NavLink></li>  
+            </ul>
+        </nav>
     )
 }
 
