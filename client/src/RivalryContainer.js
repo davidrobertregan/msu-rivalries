1b8a4a07207b442611745530d8850ef092f9a5d6
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-04 11:06:57 -0500

    rivalry card content

--- client/src/RivalryContainer.js
+++ client/src/RivalryContainer.js
@@ -18,17 +18,24 @@ function RivalryContainer( { rivalry } ) {
         })
     }, [rivalry])
 
-    console.log(games)
+    console.log(rivalry)
 
     const gameList = games.map(g => <li key={g.id}>{g.date} - {g.winning_team} - {g.score}</li>)
 
     return (
         <div>
-            <h3>
-                I am the {rivalry.name} Container...
-            </h3>
-            <div>
+            <div className="game-list">
                 {gameList}
+            </div>
+            <div className="rivalry-card">
+                <h1>{rivalry.name}</h1>
+                <h3>Overall series record</h3>
+                <img style={{maxWidth: "300px"}}src={rivalry.trophy_img_url}></img>
+                <h4>{rivalry.trophy}</h4>
+                <div>
+                    <h4>Backstory:</h4>
+                    <p>{rivalry.description}</p>
+                </div>
             </div>
         </div>
     )
