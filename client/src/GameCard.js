97d460e27d815d9691381589c8f6ec4830f8eeae
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-05 09:20:17 -0500

    favorite post fetch

--- client/src/GameCard.js
+++ client/src/GameCard.js
@@ -2,17 +2,32 @@ import { useHistory } from "react-router-dom"
 
 function GameCard( {game, setViewGame}) {
 
-const history = useHistory()
+    const history = useHistory()
+    const {winning_team, score, location, rivalry_name} = game[0]
 
-const {winning_team, score, location, rivalry_name} = game[0]
+    function handleFavoriteClick() {
+        let newFav = {
+            game_id: game[0].id
+        }
+
+        const configObj = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newFav)
+        }
+        fetch("/favorites", configObj)
+    }
 
     return(
         <div className="game-card">
-        <h1>{winning_team}</h1>
-        <h2>{score}</h2>
-        <p>{location}</p>
-        <button onClick={() => setViewGame(false)}>Back Rivalry Info</button>
-    </div>
+            <h1>{winning_team}</h1>
+            <h2>{score}</h2>
+            <p>{location}</p>
+            <button onClick={handleFavoriteClick}>⭐️</button>
+            <button onClick={() => setViewGame(false)}>Back Rivalry Info</button>
+        </div>
     )
 }
 
