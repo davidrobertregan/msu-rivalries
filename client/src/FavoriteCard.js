f044d5821ce084ef4ceef595b7baedb13d07f8f9
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-08 08:24:21 -0600

    favorite patch front end

--- client/src/FavoriteCard.js
+++ client/src/FavoriteCard.js
@@ -1,9 +1,17 @@
 import { useHistory } from "react-router-dom"
+import { useState } from 'react'
 
 function FavoriteCard( {game, setViewGame, favorites, addFavorite, deleteFavorite}) {
 
     let favorite = favorites.filter(f => f.game.id === game[0].id)[0]
     let userFavs = favorites.map(f => f.game)
+
+    const [description, setDescription] = useState(favorite.description ? favorite.description : "")
+    const [viewForm, setViewForm] = useState(false)
+
+
+    console.log(description)
+
 
     function favCheck() {
         let matches = userFavs.filter(g => g.id === game[0].id)
@@ -45,17 +53,64 @@ function FavoriteCard( {game, setViewGame, favorites, addFavorite, deleteFavorit
         favCheck() ? deleteFavFetch() : createFavFetch()
     }
 
+    function handleChange(e) {
+        setDescription(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        const body = {description: description}
+
+        const configObj = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+
+        fetch(`/favorites/${favorite.id}`, configObj)
+        .then(r => {
+            if (r.ok) {
+                r.json().then(fav => console.log(fav))
+            } else {
+                r.json().then(errors => console.log(errors))
+            }
+        })
+        setViewForm(false)
+    }
+
     const favButtonText = favCheck() ? "unfavorite" : "favorite"
 
     return(
+        <>
+
         <div className="game-card">
             <h1>Favorite Card</h1>
             <h1>{winning_team}</h1>
             <h2>{score}</h2>
             <p>{location}</p>
+            <p>Description: {description}</p>
+            <button onClick={() => {setViewForm(true)}}>edit</button>
             <button onClick={handleFavoriteClick}>{favButtonText}</button>
             <button onClick={() => setViewGame(false)}>Okay, I'm done</button>
         </div>
+
+        {viewForm ?
+
+        <form onSubmit={handleSubmit}>
+            <label>Edit description</label>
+            <input type="text" value={description} onChange={handleChange}></input>
+            <input type="submit"></input>
+        </form>
+
+        :
+        <></>
+
+        }
+
+        </>
     )
 }
 
