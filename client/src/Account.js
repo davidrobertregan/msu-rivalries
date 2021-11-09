d28cd06e68e9473c0a83696ddabb39066574b091
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-09 13:52:21 -0600

    account username change redirects to new endpoint

--- client/src/Account.js
+++ client/src/Account.js
@@ -1,10 +1,13 @@
 import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
 import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
 function Account({ currentUser, setCurrentUser }) {
 
     // what do we want to do here? delete account would be good and easy. An edit info... see recent activity would be dope! What if you could delete or edit actions from here? That's be cool too... stretch perhaps. We shoudl add a user avatar too. Extra note.
+    let history = useHistory()
+
     const [viewEditForm, setViewEditForm] = useState(false)
     const [errors, setErrors] = useState(null)
     const [formData, setFormData] = useState({ username: currentUser.username, email: currentUser.email })
@@ -43,7 +46,11 @@ function Account({ currentUser, setCurrentUser }) {
         fetch(`/users/{currentUser.id}`, configObj)
         .then(r => {
             if(r.ok) {
-                r.json().then(data => console.log(data))
+                r.json().then(user => {
+                    setCurrentUser(user);
+                    history.push(`/${user.username}`)
+                })
+                setViewEditForm(false)
             } else {
                 r.json().then(errors => setErrors(errors))
             }
