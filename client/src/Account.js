926f8435afbbafe35fbeac91a7d33bfeff1fe3ce
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-09 11:23:20 -0600

    add rivalry description in seeds, account style

--- client/src/Account.js
+++ client/src/Account.js
@@ -1,7 +1,7 @@
+import Container from "react-bootstrap/Container"
+import Button from "react-bootstrap/Button"
 
 function Account({ currentUser, setCurrentUser }) {
-
-    console.log(currentUser)
 
     // what do we want to do here? delete account would be good and easy. An edit info... see recent activity would be dope! What if you could delete or edit actions from here? That's be cool too... stretch perhaps. We shoudl add a user avatar too. Extra note.
 
@@ -16,18 +16,16 @@ function Account({ currentUser, setCurrentUser }) {
 
     let comments = currentUser.comments.map(c => <p>{c.author} commented: "{c.content}" at {c.time}</p>)
 
-    console.log(currentUser)
-
     return (
-        <div>
+        <Container style={{paddingTop: "120px"}}>
             <h1>
                 Hello, {currentUser.username}
             </h1>
             <p>{currentUser.email}</p>
-            <button onClick={handleDelete}>Delete your account</button>
             <h2>Recent Activity</h2>
             {comments}
-        </div>
+            <Button variant="danger" onClick={handleDelete}>Delete your account</Button>
+        </Container>
     )
 }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,14 +22,18 @@ puts "creating rivalries..."
     spittoon = "https://cdn.shopify.com/s/files/1/0085/8402/products/old-brass-spittoon-cover_355d77f5-e36b-4d6c-8a00-cea7f3d23697_600x.png?v=1535654017"
     land_grant = "https://i0.wp.com/www.nittanyturkey.com/wp-content/uploads/2006/11/land_grant_trophy_450.jpg"
 
+    mich_rival_desc = "The Michigan–Michigan State football rivalry is an American college football rivalry between the University of Michigan Wolverines and the Michigan State University Spartans. The teams first played in 1898 and have met 114 times. The game has now been played uninterrupted, every year since 1945. The winner of each year's game receives the Paul Bunyan – Governor of Michigan Trophy, a four-foot wooden statue of a lumberjack that was first presented in 1953 to commemorate Michigan State's beginning football competition as a member of the Big Ten Conference."
+    nd_rival_desc = "The Michigan State–Notre Dame football rivalry is an American college football rivalry between the Michigan State Spartans and Notre Dame Fighting Irish. The first game between the teams took place on November 25, 1897. Since 1949, the teams competed for the Megaphone Trophy, a trophy introduced by the Alumni Clubs of Notre Dame and Michigan State to be presented to the winner of the game. The rivalry includes several notable games, such as the 1966 game, arguably one of the greatest college football games ever played. Notre Dame currently leads the series."
+    indiana_rival_desc = "The Indiana–Michigan State football rivalry is an American college football rivalry between the Indiana Hoosiers football team of Indiana University and Michigan State Spartans football team of Michigan State University. The Old Brass Spittoon is the trophy that is annually awarded to the winner of the game. It was first presented in 1950."
+    psu_rival_desc = "When Penn State joined the Big Ten Conference in 1993, the Nittany Lions and Spartans were designated as permanent rivals, and met each other for the trophy in the last week of conference play. The trophy, designed by former Michigan State coach George Perles, features pictures of Penn State's Old Main and Michigan State's Beaumont Tower, as well as figurines of The Spartan and Nittany Lion Shrine statues. The trophy is infamous for its unwieldiness and hodgepodge appearance."
 
-    mich_rival = Rivalry.create(name: "Michigan - Michigan State", trophy: "Paul Bunyan Trophy", trophy_img_url: paul, team_one: michigan, team_two: msu, description: "The Mich-MSU description will be here!")
+    mich_rival = Rivalry.create(name: "Michigan", trophy: "Paul Bunyan Trophy", trophy_img_url: paul, team_one: michigan, team_two: msu, description: mich_rival_desc)
 
-    nd_rival = Rivalry.create(name: "Michigan State - Notre Dame", trophy: "Megaphone Trophy", trophy_img_url: megaphone, team_one: msu, team_two: notre_dame, description: "The ND - MSU description will be here!")
+    nd_rival = Rivalry.create(name: "Notre Dame", trophy: "Megaphone Trophy", trophy_img_url: megaphone, team_one: msu, team_two: notre_dame, description: nd_rival_desc)
 
-    indiana_rival = Rivalry.create(name: "Michigan State - Indiana", trophy: "Old Brass Spittoon", trophy_img_url: spittoon, team_one: indiana, team_two: msu, description: "The Indiana - MSU description will be here!")
+    indiana_rival = Rivalry.create(name: "Indiana", trophy: "Old Brass Spittoon", trophy_img_url: spittoon, team_one: indiana, team_two: msu, description: indiana_rival_desc)
 
-    psu_rival = Rivalry.create(name: "Michigan State - Penn State", trophy: "Land Grant Trophy", trophy_img_url: land_grant, team_one: psu, team_two: msu, description: "The PSU - MSU description will be here!")
+    psu_rival = Rivalry.create(name: "Penn State", trophy: "Land Grant Trophy", trophy_img_url: land_grant, team_one: psu, team_two: msu, description: psu_rival_desc)
 
 
 puts "seeding games..."
@@ -113,8 +117,8 @@ david.favorites.create(game: indiana_rival.games.last, description: "Cal Haladay
 
 puts "...creating comments"
 
-david.comments.create(game: Game.last, content: "Wow what a game!")
-meltucker.comments.create(game: Game.last, content: "Wow what a game")
-mark_dantonio.comments.create(game: Game.last, content: "Wow what a game!")
+david.comments.create(game: Game.last, content: "What a game! Go green!")
+meltucker.comments.create(game: mich_rival.games.find_by(date: "10/30/2021"), content: "Keep choppin'")
+mark_dantonio.comments.create(game: mich_rival.games.find_by(date: "10/17/2015"), content: "Whoa!!! He has trouble with the snap! And the ball is free!")
 
 puts "done ✅"
