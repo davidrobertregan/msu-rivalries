027ded7cd6706d947a7c25c10f5fb3f9de4bac61
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-10 11:37:20 -0600

    reconfig production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,7 +114,7 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { 
-:host => 'localhost', 
+:host => 'msu-rivalries.herokuapp.com', 
 :protocol => 'http'
 }
   
