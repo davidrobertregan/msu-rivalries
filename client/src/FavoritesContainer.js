aa6747b5584120138f41ffaaf97e2ba67e76a3a9
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-11 10:12:01 -0600

    one more test :)

--- client/src/FavoritesContainer.js
+++ client/src/FavoritesContainer.js
@@ -6,42 +6,10 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 function FavoritesContainer( { favorites, deleteFavorite, editFavorite } ) {
-    const [viewGame, setViewGame] = useState(false)
-    const [game, setGame] = useState(null)
-
-    let games = favorites.map(f => f.game)
-
-    function handleGameClick(e) {
-        let value = e.target.value
-        let featGame = games.filter(g => g.id === value)
-        setGame(featGame)
-        setViewGame(true)
-    }
 
     return (
         <Container style={{paddingTop: "125px"}}>
-            <Row>
-                <Col>
-                    <GameList 
-                        games={games} 
-                        handleGameClick={handleGameClick}
-                    />
-                </Col>
-
-            {viewGame ?
-                <Col>
-                    <FavoriteCard 
-                        setViewGame={setViewGame} 
-                        game={game} 
-                        favorites={favorites} 
-                        deleteFavorite={deleteFavorite} 
-                        editFavorite={editFavorite}
-                    />
-                </Col>
-            :
-                <></>
-            }
-            </Row>
+            
         </Container>  
     )
 }
