902d9f02459d38d32fdb013a70ab8d87280e2d5c
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-15 14:14:20 -0600

    carousel styling

--- client/src/About.js
+++ client/src/About.js
@@ -2,13 +2,15 @@ import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Carousel from 'react-bootstrap/Carousel'
-import Gruff from './MSU-Gruff-Sparty-Throwback-Sticker.webp'
+import Stadium from "./spartan-stadium.jpeg"
+import Duckett from "./tj-duckett.jpeg"
+import Jalen from "./jalen-watts-jackson.jpeg"
 
 function About() {
 
     return (
 
-    <Container style={{paddingTop: "125px", maxWidth: "750px"}}>
+    <Container style={{paddingTop: "125px", maxWidth: "1000px"}}>
         {/* <Row>
             <h2>
                 Welcome to Spartan Rivalries
@@ -19,34 +21,37 @@ function About() {
         <Carousel>
             <Carousel.Item>
                 <img
+                    style={{maxHeight: "500px"}}
                     className="d-block w-100"
-                    src="https://sportshub.cbsistatic.com/i/r/2018/07/06/41fc4a29-d57b-4fac-8968-eb7739f06509/thumbnail/1200x675/0349f1767374a198c162401c44d590cc/usatsi-8866786-220956-lowres.jpg"
+                    src={Jalen}
                     alt="First slide"
                 />
-                <Carousel.Caption>
+                <Carousel.Caption style={{textShadow: "2px 2px black"}}>
                 <h2><em>Explore the history...</em></h2>
-                <p>See the results to every single game in the 'Rivalries' tab and comment on your most memorable games.</p>
+                <p>See the results to every single game and comment on your most memorable games.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
             <img
+                    style={{maxHeight: "500px"}}
                     className="d-block w-100"
-                    src="https://advancelocal-adapter-image-uploads.s3.amazonaws.com/image.mlive.com/home/mlive-media/width2048/img/kzgazette/sports_impact/photo/24114849-standard.jpg"
+                    src={Duckett}
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                <h3><em>Add games to your favorites...</em></h3>
+                <Carousel.Caption style={{textShadow: "2px 2px black"}}>
+                <h2><em>Add games to your favorites...</em></h2>
                 <p>Star your most loved games and add them to your collection</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
             <img
+                    style={{maxHeight: "500px"}}
                     className="d-block w-100"
-                    src="https://www.mlive.com/resizer/xo3SFqBogHSfRdiWiuNFqDCTrqw=/1280x0/smart/advancelocal-adapter-image-uploads.s3.amazonaws.com/image.mlive.com/home/mlive-media/width2048/img/wolverines_impact/photo/devingardnerummsuhitjpg-78b03497fb4fec22.jpg"
+                    src={Stadium}
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                <h3><em>Customize your favorites...</em></h3>
+                <Carousel.Caption style={{textShadow: "2px 2px black"}}>
+                <h2><em>Customize your favorites...</em></h2>
                 <p>Record where you were, remember your favorite moment and add a custom picture</p>
                 </Carousel.Caption>
             </Carousel.Item>
