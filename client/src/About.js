58187d3b5e99a55d205c89d1e6a7ce8f67c479a6
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-11-15 11:35:10 -0600

    carousel set up about component

--- client/src/About.js
+++ client/src/About.js
@@ -1,10 +1,12 @@
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
+import Carousel from 'react-bootstrap/Carousel'
 
 function About() {
 
     return (
+
     <Container style={{paddingTop: "125px"}}>
         <Row>
             <h2>
@@ -12,20 +14,27 @@ function About() {
             </h2>
             <p><em>Relive your favorite MSU moments...</em></p>
         </Row>
-        <Row>
-            <Col>
+        
+        <Carousel variant="dark">
+            <Carousel.Item>
+                <Container className='p-5'>
                 <h3><em>Explore the history...</em></h3>
                 <p>See the results to every single game in the 'Rivalries' tab and comment on your most memorable games.</p>
-            </Col>
-            <Col>
+                </Container>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Container className='p-5'>
                 <h3><em>Add games to your favorites...</em></h3>
                 <p>Star your most loved games and add them to your collection</p>
-            </Col>
-            <Col>
+                </Container>
+            </Carousel.Item>
+            <Carousel.Item>
+                <Container className='p-5'>
                 <h3><em>Customize your favorites...</em></h3>
                 <p>Record where you were, remember your favorite moment and add a custom picture</p>
-            </Col>
-        </Row>
+                </Container>
+            </Carousel.Item>
+        </Carousel>
     </Container>
     )
 }
