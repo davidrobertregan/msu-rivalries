da697d307207670d9882ff3595fd7d1837a4fdb5
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2023-04-05 15:34:05 -0600

    render deploy config setup

---
+++ bin/render-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
+bundle exec rake db:migrate
+bundle exec rake db:seed
+# postbuild
+cp -a client/build/. public/
